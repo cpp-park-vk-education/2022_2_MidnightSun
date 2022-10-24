60f81e8f7f623bcadb15c21a769a580acd779d95
marcussss1 <danilalakalash60@gmail.com>
2022-10-24 15:34:29 +0600

    Изменил структуру

--- build/CMakeFiles/tests.dir/project/audio-player/src/square.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/tests.dir/project/audio-player/src/square.cpp.o: \
- /home/marcussss1/2022_2_MidnightSun/project/audio-player/src/square.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/tests.dir/project/net/client/src/client.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/tests.dir/project/net/client/src/client.cpp.o: \
- /home/marcussss1/2022_2_MidnightSun/project/net/client/src/client.cpp \
- /usr/include/stdc-predef.h
