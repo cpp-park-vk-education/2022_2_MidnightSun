1dd0aea4871a17db452642b39848675f1d07d9d6
marcussss1 <danilalakalash60@gmail.com>
2022-11-28 14:59:00 +0600

    HW-1

--- project/tests/test.cpp
+++ project/tests/test.cpp
@@ -2,11 +2,12 @@
 
 #include <gtest/gtest.h>
 
-TEST(MyMatrix, MatrixSubstractCol) {
-    EXPECT_EQ(square(2), 4);
-}
+// TEST(MyMatrix, MatrixSubstractCol) {
+//     EXPECT_EQ(square(2), 4);
+// }
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+    square(2);
 }
