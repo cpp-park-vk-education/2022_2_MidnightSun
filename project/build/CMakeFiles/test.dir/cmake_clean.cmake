69042f4285733cab14dbcab2f80a9f0b60b24a8d
marcussss1 <danilalakalash60@gmail.com>
2022-11-28 16:03:37 +0600

    HW-2

---
+++ project/build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/test_autogen.dir/ParseCache.txt"
+  "test_autogen"
+  "CMakeFiles/test.dir/impl/currentTrack.cpp.o"
+  "CMakeFiles/test.dir/impl/currentTrack.cpp.o.d"
+  "CMakeFiles/test.dir/impl/leftBoard.cpp.o"
+  "CMakeFiles/test.dir/impl/leftBoard.cpp.o.d"
+  "CMakeFiles/test.dir/impl/mainWindow.cpp.o"
+  "CMakeFiles/test.dir/impl/mainWindow.cpp.o.d"
+  "CMakeFiles/test.dir/impl/optionsWindow.cpp.o"
+  "CMakeFiles/test.dir/impl/optionsWindow.cpp.o.d"
+  "CMakeFiles/test.dir/main.cpp.o"
+  "CMakeFiles/test.dir/main.cpp.o.d"
+  "CMakeFiles/test.dir/test_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/test.dir/test_autogen/mocs_compilation.cpp.o.d"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
