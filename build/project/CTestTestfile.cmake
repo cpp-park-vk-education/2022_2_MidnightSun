93bf443db50ee42bc35d26724ad6f690008e62eb
marcussss1 <danilalakalash60@gmail.com>
2022-11-29 22:44:19 +0600

    Carcass

--- build/project/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcussss1/2022_2_MidnightSun/project
-# Build directory: /home/marcussss1/2022_2_MidnightSun/build/project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
